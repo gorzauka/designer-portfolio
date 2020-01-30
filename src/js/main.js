8f710c7e5fca77bbe1c878a84f414043575e4afb
Gosia Siłkowska <silkowska.m@gmail.com>
2020-01-30 19:09:55 +0100

    feat: contact form style

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,8 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+const navigationSwitcher = document.querySelector(".navigation__switcher--js");
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
-console.log(`Hello world!`);
-
-
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
